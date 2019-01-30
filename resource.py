b438ad8fe43534484773a5057b1bb7293d6ad4e7
Shubham Kamath <dev.shubhaem@gmail.com>
2019-01-31 02:17:03 +0530

    dead

--- resource.py
+++ resource.py
@@ -162,4 +162,4 @@ api.add_resource(logOut,"/logout")
 api.add_resource(userInfo,"/userinfo")
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0',port=2000)
+     app.run(host='127.0.0.1',port=2000)
